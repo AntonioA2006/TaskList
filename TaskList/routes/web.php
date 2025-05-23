ea5a702d746efba3487d976dfb75514cbe8f890a
Your Name <you@example.com>
2025-05-23 00:51:40 -0600

    Hiii i am a commit (:

--- TaskList/routes/web.php
+++ TaskList/routes/web.php
@@ -1,66 +1,25 @@
 <?php
 
+use App\Models\Task;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-class Task
-{
-    public function __construct(
-        public int $id,
-        public string $title,
-        public string $description,
-        public ?string $long_description,
-        public bool $completed,
-        public string $created_at,
-        public string $updated_at
-    ) {
-    }
-}
-
-$tasks = [
-    new Task(
-        1,
-        'Buy groceries',
-        'Task 1 description',
-        'Task 1 long description',
-        false,
-        '2023-03-01 12:00:00',
-        '2023-03-01 12:00:00'
-    ),
-    new Task(
-        2,
-        'Sell old stuff',
-        'Task 2 description',
-        null,
-        false,
-        '2023-03-02 12:00:00',
-        '2023-03-02 12:00:00'
-    ),
-    new Task(
-        3,
-        'Learn programming',
-        'Task 3 description',
-        'Task 3 long description',
-        true,
-        '2023-03-03 12:00:00',
-        '2023-03-03 12:00:00'
-    ),
-    new Task(
-        4,
-        'Take dogs for a walk',
-        'Task 4 description',
-        null,
-        false,
-        '2023-03-04 12:00:00',
-        '2023-03-04 12:00:00'
-    ),
-];
-
-
-Route::get('/', function () use($tasks) {
+Route::view('/tasks/create', 'create')->name("tasks.create");
+Route::get('/tasks', function ()  {
+    $tasks = Task::latest()->get();
     return view("index", [
         'tasks' => $tasks
     ]);
 })->name('tasks.index');
 
-Route::get("/{id}",function($id){
-    return 'return one sigle task';
+Route::get("tasks/{id}",function($id){
+
+
+    return view('show', [
+        'task' =>   Task::findOrFail($id)
+    ]);
 })->name('tasks.show');
+Route::post('/tasks', function (Request $request){
+    dd($request->all());
+})->name('tasks.store');
+
+
